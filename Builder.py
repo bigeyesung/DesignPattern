45923b610fe5408239cb3e29ad832c716fd991c6
contiliosung <sungchenhsi@gmail.com>
2019-10-27 17:19:31 +0000

    add funcs

--- Builder.py
+++ Builder.py
@@ -49,3 +49,6 @@ class Product1():
 
     def add(self, part: Any) -> None:
         self.parts.append(part)
+
+    def list_parts(self) -> None:
+        print(f"Product parts: {', '.join(self.parts)}", end="")
