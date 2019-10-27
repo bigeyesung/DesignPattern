10bac8619d656ecdbe119f570986f6bf543c8495
contiliosung <sungchenhsi@gmail.com>
2019-10-27 17:22:55 +0000

    add funcs

--- Builder.py
+++ Builder.py
@@ -78,3 +78,7 @@ if __name__ == "__main__":
     director = Director()
     builder = ConcreteBuilder1()
     director.builder = builder
+
+    print("Standard basic product: ")
+    director.build_minimal_viable_product()
+    builder.product.list_parts()
