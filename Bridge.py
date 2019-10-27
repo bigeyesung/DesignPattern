d3a13a064d4ea303dbef8301019bff3ea451b9e1
contiliosung <sungchenhsi@gmail.com>
2019-10-27 17:40:14 +0000

    add funcs

--- Bridge.py
+++ Bridge.py
@@ -2,16 +2,16 @@ from abc import ABC, abstractmethod
 
 class Abstraction:
 
-    def __init__(self, implementation: Implementation) -> None:
+    def __init__(self, implementation: Implementation):
         self.implementation = implementation
 
 
-    def operation(self) -> str:
+    def operation(self):
             return self.implementation.operation_implementation()
 
 class ExtendedAbstraction(Abstraction):
 
-    def operation(self) -> str:
+    def operation(self):
         return self.implementation.operation_implementation()
 
 class Implementation(ABC):
