5f15da91986936fa89749fa28e4fefa75e92d2ee
contiliosung <sungchenhsi@gmail.com>
2019-10-27 16:51:16 +0000

    request fun

--- Adapter.py
+++ Adapter.py
@@ -11,3 +11,6 @@ class Adapter(Target):
 
     def __init__(self, adaptee: Adaptee) -> None:
         self.adaptee = adaptee
+    
+    def request(self) -> str:
+        return f"Adapter: (TRANSLATED) {self.adaptee.specific_request()[::-1]}"
