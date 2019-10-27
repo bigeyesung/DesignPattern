3a855e7d510541deb3b15505deb34b9a4ee20a78
contiliosung <sungchenhsi@gmail.com>
2019-10-27 16:49:22 +0000

    add child class

--- Adapter.py
+++ Adapter.py
@@ -2,3 +2,9 @@ class Target():
 
     def request(self) -> str:
         return "Target: The default target's behavior."
+
+class Adaptee:
+    def specific_request(self) -> str:
+        return ".eetpadA eht fo roivaheb laicepS"
+
+class Adapter(Target):
