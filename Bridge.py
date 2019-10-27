2abda4cdcd81d7683f874e7e3edfc7badc30bb9d
contiliosung <sungchenhsi@gmail.com>
2019-10-27 17:33:43 +0000

    add funcs

--- Bridge.py
+++ Bridge.py
@@ -4,3 +4,8 @@ class Abstraction:
 
     def __init__(self, implementation: Implementation) -> None:
         self.implementation = implementation
+
+
+    def operation(self) -> str:
+            return (f"Abstraction: Base operation with:\n"
+                    f"{self.implementation.operation_implementation()}")
