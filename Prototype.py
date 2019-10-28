3a2b7c82d109effd2fa7555fd4ea3605585115a6
contiliosung <sungchenhsi@gmail.com>
2019-10-28 22:36:46 +0000

    add funcs

--- Prototype.py
+++ Prototype.py
@@ -3,3 +3,8 @@ from copy import deepcopy
 from typing import Any
 
 class Prototype:
+
+    def __init__(self) -> None:
+        self._primitive = None
+        self._component = None
+        self._circular_reference = None
