318475debe037b8b38a83c2a3f4676d15bdb1f3e
contiliosung <sungchenhsi@gmail.com>
2019-10-29 23:04:00 +0000

    add funcs

--- Decorator.py
+++ Decorator.py
@@ -38,3 +38,8 @@ if __name__ == "__main__":
     print("Client: I've got a simple component:")
     client_code(simple)
     print("\n")
+
+    decorator1 = ConcreteDecoratorA(simple)
+    decorator2 = ConcreteDecoratorB(decorator1)
+    print("Client: Now I've got a decorated component:")
+    client_code(decorator2)
