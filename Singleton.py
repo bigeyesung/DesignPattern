022afad3b3059c6cf308892c075e40b16bbcf044
contiliosung <sungchenhsi@gmail.com>
2019-10-30 23:17:01 +0000

    add funcs

--- Singleton.py
+++ Singleton.py
@@ -3,3 +3,6 @@ class SingletonMeta(type):
     _instance: Optional[Singleton] = None
 
     def __call__(self) -> Singleton:
+        if self._instance is None:
+            self._instance = super().__call__()
+        return self._instance
