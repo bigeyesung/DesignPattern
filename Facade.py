a96288a1b485d3c86dd4a24cc40415e41ae062bd
contiliosung <sungchenhsi@gmail.com>
2019-10-31 23:00:55 +0000

    add funcs

--- Facade.py
+++ Facade.py
@@ -2,3 +2,5 @@ from __future__ import annotations
 class Facade:
 
     def __init__(self, subsystem1: Subsystem1, subsystem2: Subsystem2) -> None:
+        self._subsystem1 = subsystem1 or Subsystem1()
+        self._subsystem2 = subsystem2 or Subsystem2()
