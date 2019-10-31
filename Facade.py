4cae08b5362dc1520aa8f53686d996a2457ddff4
contiliosung <sungchenhsi@gmail.com>
2019-10-31 23:05:19 +0000

    add funcs

--- Facade.py
+++ Facade.py
@@ -34,3 +34,7 @@ def client_code(facade: Facade) -> None:
     print(facade.operation(), end="")
 
 if __name__ == "__main__":
+    subsystem1 = Subsystem1()
+    subsystem2 = Subsystem2()
+    facade = Facade(subsystem1, subsystem2)
+    client_code(facade)
