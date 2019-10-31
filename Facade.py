b9760710dec5ce363f910bc556d0424c08f5a7ef
contiliosung <sungchenhsi@gmail.com>
2019-10-31 23:01:14 +0000

    add funcs

--- Facade.py
+++ Facade.py
@@ -4,3 +4,5 @@ class Facade:
     def __init__(self, subsystem1: Subsystem1, subsystem2: Subsystem2) -> None:
         self._subsystem1 = subsystem1 or Subsystem1()
         self._subsystem2 = subsystem2 or Subsystem2()
+
+    def operation(self) -> str:
