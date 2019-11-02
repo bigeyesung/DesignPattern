62d47f47f3b6992f63213422bea0b4388b58d65a
contiliosung <sungchenhsi@gmail.com>
2019-11-02 16:37:21 +0000

    add funcs

--- Proxy.py
+++ Proxy.py
@@ -2,32 +2,32 @@ from abc import ABC, abstractmethod
 class Subject(ABC):
 
     @abstractmethod
-    def request(self) -> None:
+    def request(self):
         pass
 
 class RealSubject(Subject):
 
-    def request(self) -> None:
+    def request(self):
         print("RealSubject: Handling request.")
 
 class Proxy(Subject):
 
-    def __init__(self, real_subject: RealSubject) -> None:
+    def __init__(self, real_subject: RealSubject):
         self._real_subject = real_subject
 
-    def request(self) -> None:
+    def request(self):
         if self.check_access():
             self._real_subject.request()
             self.log_access()
 
-    def check_access(self) -> bool:
+    def check_access(self):
         print("Proxy: Checking access prior to firing a real request.")
         return True
 
-    def log_access(self) -> None:
+    def log_access(self):
         print("Proxy: Logging the time of request.", end="")
 
-def client_code(subject: Subject) -> None:
+def client_code(subject: Subject):
     subject.request()
 
 if __name__ == "__main__":
@@ -37,3 +37,7 @@ if __name__ == "__main__":
     client_code(real_subject)
 
     print("")
+
+    print("Client: Executing the same client code with a proxy:")
+    proxy = Proxy(real_subject)
+    client_code(proxy)
