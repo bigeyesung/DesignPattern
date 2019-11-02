c695cfecee34fa9245e815b6862557d6e5f04709
contiliosung <sungchenhsi@gmail.com>
2019-11-02 16:32:36 +0000

    add funcs

--- Proxy.py
+++ Proxy.py
@@ -4,5 +4,8 @@ class Subject(ABC):
     @abstractmethod
     def request(self) -> None:
         pass
-    
+
 class RealSubject(Subject):
+
+    def request(self) -> None:
+        print("RealSubject: Handling request.")
