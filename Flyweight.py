398ad1bdc1176dd475b2dfa1dcdf9e49abd0d5a8
contiliosung <sungchenhsi@gmail.com>
2019-11-02 12:10:18 +0000

    add funcs

--- Flyweight.py
+++ Flyweight.py
@@ -4,3 +4,8 @@ from typing import Dict
 class Flyweight():
     def __init__(self, shared_state: str) -> None:
         self._shared_state = shared_state
+
+    def operation(self, unique_state: str) -> None:
+            s = json.dumps(self._shared_state)
+            u = json.dumps(unique_state)
+            print(f"Flyweight: Displaying shared ({s}) and unique ({u}) state.", end="")
