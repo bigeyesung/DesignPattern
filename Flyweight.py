bc3db4330866b4107333c5aef7da6d66fffe1436
contiliosung <sungchenhsi@gmail.com>
2019-11-02 12:17:09 +0000

    add funcs

--- Flyweight.py
+++ Flyweight.py
@@ -2,10 +2,10 @@ import json
 from typing import Dict
 
 class Flyweight():
-    def __init__(self, shared_state: str) -> None:
+    def __init__(self, shared_state: str):
         self._shared_state = shared_state
 
-    def operation(self, unique_state: str) -> None:
+    def operation(self, unique_state: str):
             s = json.dumps(self._shared_state)
             u = json.dumps(unique_state)
             print(f"Flyweight: Displaying shared ({s}) and unique ({u}) state.", end="")
@@ -13,15 +13,15 @@ class Flyweight():
 class FlyweightFactory():
     _flyweights: Dict[str, Flyweight] = {}
 
-    def __init__(self, initial_flyweights: Dict) -> None:
+    def __init__(self, initial_flyweights: Dict):
             for state in initial_flyweights:
                 self._flyweights[self.get_key(state)] = Flyweight(state)
 
-    def get_key(self, state: Dict) -> str:
+    def get_key(self, state: Dict):
 
         return "_".join(sorted(state))
 
-    def get_flyweight(self, shared_state: Dict) -> Flyweight:
+    def get_flyweight(self, shared_state: Dict):
         key = self.get_key(shared_state)
 
         if not self._flyweights.get(key):
@@ -32,7 +32,7 @@ class FlyweightFactory():
 
         return self._flyweights[key]
 
-    def list_flyweights(self) -> None:
+    def list_flyweights(self):
         count = len(self._flyweights)
         print(f"FlyweightFactory: I have {count} flyweights:")
         print("\n".join(map(str, self._flyweights.keys())), end="")
@@ -40,7 +40,7 @@ class FlyweightFactory():
 def add_car_to_police_database(
     factory: FlyweightFactory, plates: str, owner: str,
     brand: str, model: str, color: str
-) -> None:
+    ):
 
     print("\n\nClient: Adding a car to database.")
     flyweight = factory.get_flyweight([brand, model, color])
